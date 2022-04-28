dec9465c023c80e869142e6c018249426445b7c4
Khayman King <khaymanak@gmail.com>
2022-04-28 14:58:23 -0700

    background confusing

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -6,6 +6,10 @@ import AnimalList from '../AnimalList/AnimalList.js';
 export default function Main({ animals }) {
   return (
     <main style={{ 
+      height: 'calc(100vh - 50px)',
+      'margin-bottom': '40px',   
+      'background-repeat': 'no-repeat',
+      background: 'auto',
       backgroundImage: `url(${background})` }}>
       <AnimalList animals={animals}/> 
     </main>
