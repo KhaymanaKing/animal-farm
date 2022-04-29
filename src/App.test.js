b5334dd772aea75416c8ef9a61e46c1f638a8d27
Khayman King <khaymanak@gmail.com>
2022-04-29 10:13:02 -0700

    refixed test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/goose/i);
+  const linkElement = screen.getByText('goose');
   expect(linkElement).toBeInTheDocument();
 });
