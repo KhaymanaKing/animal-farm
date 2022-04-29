a086ad69c0238c373c48a19d731f0a4ce56de523
Khayman King <khaymanak@gmail.com>
2022-04-29 09:27:43 -0700

    fixed main.js to pass netlify test

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import background from '../background.png';
-import './Main.css';
 import AnimalList from '../AnimalList/AnimalList.js';
 
 export default function Main({ animals }) {
